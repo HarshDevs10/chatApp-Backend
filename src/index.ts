3d0bec79be4a8cb4f51b853c069b8f9ae71131b0
HarshDevs10 <harshdevsinghthakur8@gmail.com>
2025-11-01 17:36:12 +0000

    Chatapp logic

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,18 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8000})
 let allSocket: WebSocket[] = []
-let Usercount = 0
 
 wss.on('connection', (Socket) => {
-    Usercount++
-    console.log("Connected #" + Usercount)
-    allSocket.push(Socket)
 
     Socket.on('message', (e) => {
         setTimeout(() => {
             allSocket.forEach((client) => {
-                client.send(e.toString() + "send from the server")
+                client.send(e.toString())
             })
-        }, 1000)
+        }, 500)
     })
 
     Socket.on('close', () => {
-        Usercount--
+        allSocket = allSocket.filter(x => x != Socket)
     })
 })
