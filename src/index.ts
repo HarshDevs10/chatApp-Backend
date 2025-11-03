040d42d14c56ba6658a94488a2f313886ea135ef
HarshDevs10 <harshdevsinghthakur8@gmail.com>
2025-11-03 10:24:00 +0000

    Logic for Broadcasting but no resending to socket

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,15 @@ const wss = new WebSocketServer({ port: 8000})
 let allSocket: WebSocket[] = []
 
 wss.on('connection', (Socket) => {
+    allSocket.push(Socket)
 
     Socket.on('message', (e) => {
-        setTimeout(() => {
-            allSocket.forEach((client) => {
+          allSocket.forEach((client) => {
+            if(client != Socket){
                 client.send(e.toString())
-            })
-        }, 500)
-    })
+            }
+          })
+        })
 
     Socket.on('close', () => {
         allSocket = allSocket.filter(x => x != Socket)
