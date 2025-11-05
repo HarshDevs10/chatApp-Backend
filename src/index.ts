a01ab68c811e6e4bde7a645e98cc92ce12c04a13
HarshDevs10 <harshdevsinghthakur8@gmail.com>
2025-11-05 11:36:29 +0000

    Added room Logic

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,41 @@
 import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8000})
-let allSocket: WebSocket[] = []
+
+interface User {
+    socket: WebSocket
+    room: string
+}
+let allSocket: User[] = []
 
 wss.on('connection', (Socket) => {
-    allSocket.push(Socket)
 
-    Socket.on('message', (e) => {
-          allSocket.forEach((client) => {
-            if(client != Socket){
-                client.send(e.toString())
-            }
-          })
+    Socket.on('message', (message) => {
+        const ParsedMessage = JSON.parse(message.toString())
+
+        if(ParsedMessage.type === "join"){
+            allSocket.push({
+                socket: Socket,
+                room: ParsedMessage.payload.roomId
+            })
+            Socket.send("Joined the Room " + ParsedMessage.payload.roomId)
+        }
+        else if(ParsedMessage.type === "chat"){
+
+            const ele = allSocket.find(x => x.socket === Socket)
+            if (!ele) return Socket.send("Join a Room first.")
+            const roomId = ele.room
+
+            allSocket.forEach((e) => {
+                if(e.room === roomId){
+                    e.socket.send(ParsedMessage.payload.message)
+                }
+            })
+            
+        }
         })
 
     Socket.on('close', () => {
-        allSocket = allSocket.filter(x => x != Socket)
+        allSocket = allSocket.filter(x => x.socket != Socket)
     })
 })
